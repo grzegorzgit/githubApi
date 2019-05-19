092829858b11e2cdfc97e5e142e3b535618abedf
Greg <>
2019-05-20 00:46:12 +0200

    Formating.

--- src/js/Header.js
+++ src/js/Header.js
@@ -7,12 +7,11 @@ export default class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: null,
-            companyname: null
+            username: this.props.user
         }
 
         this.inputVal = this.inputVal.bind(this);
-        this.loadRepos  = this.loadRepos.bind(this);
+        this.loadRepos = this.loadRepos.bind(this);
     }
 
     inputVal(e) {
@@ -21,18 +20,18 @@ export default class Header extends Component {
         })
     }
 
-    loadRepos(e){
+    loadRepos(e) {
         e.preventDefault();
         this.props.loadRepos(this.state.username)
     }
-  
+
     render() {
         return (
             <header>
                 <form onSubmit={this.loadRepos}>
                     <div className="inputs">
                         <h1>Please provide a username or company name:</h1>
-                        <input type="text" placeholder="User name" name="username" id="username" onChange={this.inputVal} /> 
+                        <input type="text" placeholder="User name" name="username" id="username" onChange={this.inputVal} />
                         <button type="submit" onClick={this.loadRepos}>Get repositories</button>
                     </div>
                 </form>
